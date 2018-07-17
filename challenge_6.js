1856715c1b2d6054e483a1157805bf957883b27f
gchu <gchu647@gmail.com>
2018-07-17 10:12:41 -1000

    Finish

--- challenge_6.js
+++ challenge_6.js
@@ -4,13 +4,34 @@
  *  @method sumOfSquares()       returns ths sum of all 'naturalNumbers' squared
  *  @method squareOfTheSums()    returns the square of all 'naturalNumbers'
  */
-exports.SumOfASquare = SumOfASquare;
 
-function SumOfASquare(naturalNumbers){
-  this.naturalNumbers = naturalNumbers;
+exports.SumOfASquare = class SumOfASquare {
+  constructor(naturalNumbers) {
+    this.naturalNumbers = naturalNumbers;
+  }
+
+  sumOfSquares() {
+    let sum = 0;
+    
+    for (let i = 1; i <= this.naturalNumbers; i++) {
+      sum += Math.pow(i, 2);      
+    }
+  
+    return sum;
+  }
+
+  squareOfTheSums() {
+    let sum = 0;
+    let square = 0;
+
+    for (let j = 1; j <= this.naturalNumbers; j++) {
+      sum += j;
+    }
+
+    square = Math.pow(sum, 2);
+    return square;
+  }
 }
-
-// do work here
 
 // overwrite the object prototype, or add methods to the prototype
 // to satisfy the two method calls, sumOfSquares() and squareOfTheSums()
